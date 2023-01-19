fe85fe56d0d7252ccc4944b4c0ec61953a4523d1
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-01-19 10:52:02 -0600

    Update logo.js

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,12 +1,27 @@
+// import React from 'react';
+
+// import '../../main.css';
+// import './logo.css'
+
+// import logo from './logo.png'
+
+// export default function Logo() {
+//   return (
+//     <img src={logo} />
+//   );
+// }
+
+
 import React from 'react';
 
 import '../../main.css';
 import './logo.css'
 
-import logo from './logo.png'
-
 export default function Logo() {
+  const logo = '{-}'
   return (
-    <img src={logo} />
+    <div>
+      <p className='pointer no-select | logo'>{logo}</p>
+    </div>
   );
 }
