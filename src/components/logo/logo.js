4caedf70273204dffdc8c82be1c3a450c4dea1da
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-01-19 10:11:35 -0600

    1 file modified, 1 added, 1 deleted

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import '../../main.css';
-import './logo.css'
+import logo from './logo.png'
 
 export default function Logo() {
   return (
-    <img src="../../assets/images/TemplateAPI Logo.png" />
+    <img src={logo} />
   );
+  
 }
