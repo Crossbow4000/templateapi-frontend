eca66335995851a6a69cc5b2cad9df1ddea8d93e
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-01-19 11:00:32 -0600

    Update logo.js

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,27 +1,27 @@
-// import React from 'react';
-
-// import '../../main.css';
-// import './logo.css'
-
-// import logo from './logo.png'
-
-// export default function Logo() {
-//   return (
-//     <img src={logo} />
-//   );
-// }
-
-
 import React from 'react';
 
 import '../../main.css';
 import './logo.css'
 
+import logo from './logo.png'
+
 export default function Logo() {
-  const logo = '{-}'
   return (
-    <div>
-      <p className='pointer no-select | logo'>{logo}</p>
-    </div>
+    <img src={logo} />
   );
 }
+
+
+// import React from 'react';
+
+// import '../../main.css';
+// import './logo.css'
+
+// export default function Logo() {
+//   const logo = '{-}'
+//   return (
+//     <div>
+//       <p className='pointer no-select | logo'>{logo}</p>
+//     </div>
+//   );
+// }
