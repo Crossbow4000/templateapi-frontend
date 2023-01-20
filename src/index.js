3a30dccce760643d3ebe4d1e1661ec01ca2ab3fe
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-01-20 11:21:02 -0600

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from './pages/home-page/home-page.js';
 import Dashboard from './pages/dashboard/dashboard.js'
@@ -8,7 +9,10 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <HomePage />
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<HomePage/>}></Route>
+      <Route path="dashboard" element={<Dashboard/>}></Route>
+    </Routes>
+  </BrowserRouter>
 );
