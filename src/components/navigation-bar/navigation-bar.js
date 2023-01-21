0cef4195000d4dc4d1b64e4a5908534b60203293
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-01-20 20:48:22 -0600

    2 files modified

--- src/components/navigation-bar/navigation-bar.js
+++ src/components/navigation-bar/navigation-bar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Outlet, Link } from "react-router-dom";
+
 import '../../main.css';
 import './navigation-bar.css'
 
@@ -14,6 +16,7 @@ export default function NavigationBar(props) {
         {props.page == 'home'      ? <PageLink name="Home" isCurrentPage="true" />      : <PageLink name="Home" /> }
         {props.page == 'dashboard' ? <PageLink name="Dashboard" isCurrentPage="true" /> : <PageLink name="Dashboard" /> }
       </div>
+      <Outlet/>
     </div>
   )
 }
@@ -21,18 +24,27 @@ export default function NavigationBar(props) {
 
 
 function PageLink(props) {
+
+  let link
+  if(props.name.toLowerCase() == 'home') {
+    link = '/'
+  } else {
+    link = `/${props.name.toLowerCase()}`
+  }
+
   if (props.isCurrentPage == 'true') {
     return (
       <div className={'pointer | page-link'}>
-        <p className={'no-select | page-link-text'}>{props.name}</p>
+        <Link to={link} className={'no-select | page-link-text'}>{props.name}</Link>
         <div className={'page-link-is-current-page'}></div>
       </div>
     )
   } else {
     return (
       <div className={'pointer | page-link'}>
-        <p className="no-select | page-link-text">{props.name}</p>
+        <Link to={link} className={'no-select | page-link-text'}>{props.name}</Link>
       </div>
     )
   }
+
 }
