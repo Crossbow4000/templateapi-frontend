480353f1501a45e966589cada85a31045cb2c50b
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-01-22 17:00:23 -0600

    2 files modified, 3 added, 3 deleted

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomePage from './pages/home-page/home-page.js';
+import LandingPage from './pages/landing-page/landing-page.js';
 import Dashboard from './pages/dashboard/dashboard.js'
 
 const rootElement = document.getElementById('root');
@@ -11,7 +11,7 @@ const root = createRoot(rootElement);
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route index element={<HomePage/>}></Route>
+      <Route index element={<LandingPage/>}></Route>
       <Route path="dashboard" element={<Dashboard/>}></Route>
     </Routes>
   </BrowserRouter>
--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -3,11 +3,11 @@ import React from 'react';
 import NavigationBar from '../../components/navigation-bar/navigation-bar.js'
 import MainSection from './components/main-section/main-section.js'
 
-export default function HomePage() {
+export default function LandingPage() {
   return (
     <div>
-      <NavigationBar page="home" />
-      <MainSection />
+      <NavigationBar page="home"/>
+      <MainSection/>
     </div>
   )
 }
