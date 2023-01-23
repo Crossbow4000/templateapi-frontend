50d75b54f9937c6169ebd674984c4c9b482be114
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-01-23 09:05:30 -0600

    1 file modified

--- src/pages/landing-page/components/main-section/main-section.js
+++ src/pages/landing-page/components/main-section/main-section.js
@@ -1,24 +1,24 @@
 import React from 'react';
+import { useRef } from 'react'
 
 import '../../../../main.css'
 import './main-section.css'
 
 export default function MainSection() {
+  const featuresSection = useRef()
+
   return (
     <div>
-      <HeroSection/>
-      <Features/>
+      <HeroSection featuresRef={featuresSection}/>
+      <Features featuresRef={featuresSection}/>
     </div>
   )
 }
 
-function HeroSection() {
-  function ScrollToFeatures() {
-    let featuresSection = document.getElementById('features')
-    if(featuresSection) {
-      featuresSection.scrollIntoView()
-    }
-  }
+function HeroSection(props) {
+  const ScrollToFeatures = () => {
+    props.featuresRef.current?.scrollIntoView({behavior: 'smooth'});
+  };
 
   return (
     <div className={"grid | hero-section"}>
@@ -28,16 +28,16 @@ function HeroSection() {
         <button className={"no-select | outline-button"}>Login</button>
         <button className={"no-select | gradient-button"}>Sign Up</button>
       </div>
-      <button onClick={ScrollToFeatures()}><svg className={"scroll-button-icon"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg></button>
+      <button onClick={ScrollToFeatures}><svg className={"scroll-button-icon"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg></button>
     </div>
   )
 }
 
-function Features() {
+function Features(props) {
   return (
     
     <div className={"flex"}>
-      <div className={"grid | features"} id={"features"}>
+      <div className={"grid | features"} id={"features"} ref={props.featuresRef}>
 
         <div className={"grid | feature"}>
           <svg className={"feature-icon"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2v82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9V380.8c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168c22.1 0 40-17.9 40-40s-17.9-40-40-40s-40 17.9-40 40s17.9 40 40 40z"/></svg>
